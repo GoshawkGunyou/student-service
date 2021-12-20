f2e25a05f69689d3efb0374da9fedf0fb2dc0106
GoshawkGunyou <62436931+GoshawkGunyou@users.noreply.github.com>
2021-12-20 16:21:36 +0800

    Grades Query complete

--- src/main/java/com/example/studentservice/controller/HomeController.java
+++ src/main/java/com/example/studentservice/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.example.studentservice.controller;
 
+import com.example.studentservice.domain.student.Student;
+import com.example.studentservice.dto.Query;
 import com.example.studentservice.service.ClassInfoService;
+import com.example.studentservice.service.GradeService;
+import com.example.studentservice.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,9 +15,19 @@ public class HomeController {
     @Autowired
     ClassInfoService classInfoService;
 
+    @Autowired
+    GradeService gradeService;
+
     @RequestMapping("/display")
     public String toGradeQuery(Model model) {
         model.addAttribute("classInfos", classInfoService.findClasses());
         return "gradeQuery";
+    }
+
+    @RequestMapping("display/student")
+    public String toGrade(Model model, Query query) {
+        System.out.println(query);
+        model.addAttribute("studentGradeDTO", gradeService.getGradeOf(query.getStudentName(), query.getMin(), query.getMax()));
+        return "studentGrade";
     }
 }
