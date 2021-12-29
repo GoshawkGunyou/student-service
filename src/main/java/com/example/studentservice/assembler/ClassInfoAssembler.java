af33ec35f63baaaf38b95c15145bcb05b5c6e456
GoshawkGunyou <62436931+GoshawkGunyou@users.noreply.github.com>
2021-12-29 14:35:13 +0800

    classInfoDTO grade average refactor

--- src/main/java/com/example/studentservice/assembler/ClassInfoAssembler.java
+++ src/main/java/com/example/studentservice/assembler/ClassInfoAssembler.java
@@ -12,47 +12,12 @@ public class ClassInfoAssembler {
         ClassInfoDTO classInfoDTO = new ClassInfoDTO();
         // Averages list of Math, English, Language and Total average values in specified order.
         if (gradeList != null) {
-            List<Double> averages = doAverages(gradeList);
-            classInfoDTO.setMathAvg(averages.get(0));
-            classInfoDTO.setEnglishAvg(averages.get(1));
-            classInfoDTO.setLanguageAvg(averages.get(2));
-            classInfoDTO.setTotalAvg(averages.get(3));
+            classInfoDTO.doAverages(gradeList);
         }
         classInfoDTO.setClassSerial(classInfo.getSerial());
         classInfoDTO.setClassName(classInfo.getName());
         return classInfoDTO;
     }
 
-    private static List<Double> doAverages(List<Grade> gradeList) {
-        ArrayList<Double> output = new ArrayList<>(4);
-        int mathCount = 0;
-        int languageCount = 0;
-        int englishCount = 0;
-        double mathSum = 0.0;
-        double languageSum = 0.0;
-        double englishSum = 0.0;
 
-        for (Grade grade : gradeList) {
-            Double math;
-            Double english;
-            Double language;
-            if ((math = grade.getMath()) != null) {
-                mathSum += math;
-                mathCount++;
-            }
-            if ((english = grade.getEnglish()) != null) {
-                englishSum += english;
-                englishCount++;
-            }
-            if ((language = grade.getLanguage()) != null) {
-                languageSum += language;
-                languageCount++;
-            }
-        }
-        output.add(0, mathSum / mathCount);
-        output.add(1, englishSum / englishCount);
-        output.add(2, languageSum / languageCount);
-        output.add(3, (mathSum + languageSum + englishSum) / (mathCount + languageCount + englishCount));
-        return output;
-    }
 }
--- src/main/java/com/example/studentservice/dto/ClassInfoDTO.java
+++ src/main/java/com/example/studentservice/dto/ClassInfoDTO.java
@@ -1,8 +1,11 @@
 package com.example.studentservice.dto;
 
+import com.example.studentservice.domain.grade.Grade;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Data
@@ -17,5 +20,36 @@ public class ClassInfoDTO {
 
     public void setClassSerial(String classSerial) {
         this.classSerial = "CL-" + classSerial;
+    }
+
+    public void doAverages(List<Grade> grades) {
+        int mathCount = 0;
+        int languageCount = 0;
+        int englishCount = 0;
+        double mathSum = 0.0;
+        double languageSum = 0.0;
+        double englishSum = 0.0;
+
+        for (Grade grade : grades) {
+            Double math;
+            Double english;
+            Double language;
+            if ((math = grade.getMath()) != null) {
+                mathSum += math;
+                mathCount++;
+            }
+            if ((english = grade.getEnglish()) != null) {
+                englishSum += english;
+                englishCount++;
+            }
+            if ((language = grade.getLanguage()) != null) {
+                languageSum += language;
+                languageCount++;
+            }
+        }
+        mathAvg = mathSum / mathCount;
+        englishAvg = englishSum / englishCount;
+        languageAvg =  languageSum / languageCount;
+        totalAvg = (mathSum + languageSum + englishSum) / (mathCount + languageCount + englishCount);
     }
 }
